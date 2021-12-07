75261fea3badcc8c14415894da87643330dfe6db
Manan Gupta <71245195+manang1803@users.noreply.github.com>
2021-12-07 15:06:23 +0100

    Final estimator.py
    this is the final file with an RMSE of 0.667

--- estimator.py
+++ estimator.py
@@ -10,6 +10,9 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.preprocessing import StandardScaler
 import xgboost as xgb
+from skopt import BayesSearchCV
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import RandomizedSearchCV
 
 def _encode_dates(X):
     X = X.copy()  # modify a copy of X
@@ -29,6 +32,7 @@ def _encode_dates(X):
     
     # Finally we can drop the original columns from the dataframe
     return X.drop(columns=["date", 'month','weekday','hour']) 
+    
 
 def _merge_external_data(X):
     file_path = Path(__file__).parent / 'external_data.csv'
@@ -47,23 +51,23 @@ def get_estimator():
     date_encoder = FunctionTransformer(_encode_dates)
     date_cols = ['year', 'day', 'hour_sin', 'hour_cos', 'month_sin', 'month_cos', 'weekday_sin', 'weekday_cos']
     categorical_cols = ["counter_name", "site_name","counter_id",'site_id']
-    numerical_cols = numeric_cols = ['t','rr3', 'ff','vv','hbas','rafper','paris_covid','pm10','parks_change', 'residential_change', 'grocery_change', 'workplace_change','stations_change','retail_change','flow_traffic','traffic_occupation','stringency_index','vehicle_number']
+    numerical_cols = ['t','rr3', 'ff','vv','hbas','rafper','paris_covid','pm10','parks_change', 'residential_change', 'grocery_change', 'workplace_change','stations_change','retail_change','flow_traffic','traffic_occupation','stringency_index','vehicle_number']
     
     preprocessor = ColumnTransformer([
         ('date', "passthrough", date_cols),
         ('cat', OneHotEncoder(handle_unknown = 'ignore'), categorical_cols),
         ('numeric', StandardScaler(), numerical_cols),
     ])
-    
+
     pipe = make_pipeline(FunctionTransformer(_merge_external_data, validate=False),date_encoder, preprocessor,
              xgb.XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-             colsample_bynode=1, colsample_bytree=0.8790730386941, gamma=0.11903774537530729, gpu_id=-1,
+             colsample_bynode=1, colsample_bytree=0.8790730386941, gamma=0.41903774537530729, gpu_id=-1,
              importance_type='gain', interaction_constraints='',eval_metric='rmse',
              eta=0.05817619370698, max_depth=8, 
              min_child_weight=24.9870361960816, objective= 'reg:squarederror',
              n_estimators=705, n_jobs=8, num_parallel_tree=1, random_state=0,
-             reg_alpha=5.3603597158578445, reg_lambda=1.3954299611600072, scale_pos_weight=1,
-             subsample=0.6186297135971556, tree_method='exact', validate_parameters=1,
+             reg_alpha=6.3603597158578445, reg_lambda=2.3954299611600072, scale_pos_weight=1,
+             subsample=0.6586297135971556, tree_method='exact', validate_parameters=1,
              verbosity=None))
 
     return pipe
